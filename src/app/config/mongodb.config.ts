0779cac2674ae931798aa948a2474466bfbdf0a8
LucasChch <yolucasch99@gmail.com>
2025-04-14 19:57:44 -0300

    Tenemos conexión a mongodb

---
+++ src/app/config/mongodb.config.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectMongoDB = async () => {
+   try {
+     await mongoose.connect("mongodb://admin:admin@localhost:27017/"); //TODO: Cambiarlo por una variable de entorno
+     console.log('Conectado a la base de datos');
+   } catch (error) {
+     console.error('Error al conectar a la base de datos', error);
+     process.exit(1);
+   }
+ };
+ 
+ export default connectMongoDB;
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes/index';
+import connectMongoDB from './config/mongodb.config';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+//conexión a la base de datos
+connectMongoDB();
+
 app.use(routes);
+
+
 
 app.listen(3000, () => {
    console.log('Server running on port 3000');
