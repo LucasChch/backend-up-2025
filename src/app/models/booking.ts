95c90e946684d41f4ad8bcc9ce9a8059d0de619f
LucasChCh <yolucasch99@gmail.com>
2025-04-17 19:14:07 -0300

    bookingItem y safetyItem se embeben en booking

--- src/app/models/booking.ts
+++ src/app/models/booking.ts
@@ -1,16 +1,27 @@
 import mongoose from 'mongoose';
 
+const safetyItemSchema = new mongoose.Schema({
+   type: { type: String, enum: ['casco', 'chaleco'], required: true },
+   quantity: { type: Number, default: 1 }
+ });
+ 
+ const bookingItemSchema = new mongoose.Schema({
+   productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+   quantity: { type: Number, default: 1 },
+   safetyItems: [safetyItemSchema]
+ });
+ 
+
 const BookingSchema = new mongoose.Schema({
    customerId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Customer',
       required: true
    },
-   items: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'BookingItem',
-      required: true
-   }],
+   items: {
+      type: [bookingItemSchema],
+      required: true,
+   },
    createdAt: {
       type: Date,
       default: Date.now
@@ -21,7 +32,9 @@ const BookingSchema = new mongoose.Schema({
    },
    turns: {
       type: Number,
-      required: true
+      required: true,
+      min: 1,
+      max: 3
    },
    subTotal: {
       type: Number,
