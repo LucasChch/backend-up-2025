80a53635470302790da09f8d5d2e2e17e752a5e0
LucasChch <yolucasch99@gmail.com>
2025-06-09 00:01:00 -0300

    validación de usuario por mail y regex para el teléfono

--- src/app/repositories/customer.ts
+++ src/app/repositories/customer.ts
@@ -12,3 +12,7 @@ export const createCustomer = async (customerData: any) => {
    const customer = new Customer(customerData);
    return await customer.save();
 };
+
+export const getCustomerByEmail = async (email: string) => {
+   return await Customer.findOne({ 'email': email });
+};
--- src/app/services/customer.ts
+++ src/app/services/customer.ts
@@ -1,3 +1,5 @@
+import customer from '../models/customer'
+import { ValidationError } from '../models/errors'
 import * as CustomerRepository from '../repositories/customer'
 
 export const getAllCustomers = async () => {
@@ -9,5 +11,25 @@ export const getCustomerById = async (customerId:string) => {
 }
 
 export const createCustomer = async (customerData:any) => {
+
+   if (customerData.email){
+      // contiene @
+      if (!customerData.email.includes('@')) {
+         throw new ValidationError('El correo electrónico no es válido');
+      }
+      const existingCustomer = await CustomerRepository.getCustomerByEmail(customerData.email);
+      if (existingCustomer) {
+         throw new ValidationError('El correo electrónico ya está en uso');
+      }
+   }
+
+   if(customerData.phone){
+      // validación regex para el teléfono
+      // permite números, +, -, espacios y paréntesis
+      const phoneRegex = /^[0-9+\-\s\(\)]+$/;
+      if (!phoneRegex.test(customerData.phone)) {
+         throw new ValidationError('El número de teléfono no es válido');
+      }
+   }
    return await CustomerRepository.createCustomer(customerData)
 }
